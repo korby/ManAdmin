c5cbd4c54d24398081cf1a1c2efaf7713035165b
a.cianfarani <a.cianfarani@c2is.fr>
2015-09-12 14:28:20 +0200

    Languages' dir ref changed to work also with symlinks

--- man-admin.php
+++ man-admin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: ManAdmin
 Description: A simple back office usage's manual area, accessible by users and editable by administrators. Compatibility WP >= 3.8
-Version: 0.1
+Version: 0.2
 Author: André Cianfarani
 License: GPL2
 */
@@ -13,8 +13,7 @@ register_activation_hook(__FILE__, array('Manadmin', 'activate'));
 register_uninstall_hook(__FILE__, array('Manadmin', 'uninstall'));
 
 function man_admin_text_domain() {
-    load_plugin_textdomain( 'man-admin', plugin_dir_path( __FILE__ ) . 'languages', basename( dirname( __FILE__ ) ) . '/languages');
+    load_plugin_textdomain( 'man-admin', plugin_dir_path( __FILE__ ) . 'languages', dirname(plugin_basename(__FILE__)).'/languages');
 }
-
 
 add_action( 'init', 'man_admin_text_domain' );
--- readme.txt
+++ readme.txt
@@ -8,7 +8,7 @@ Stable tag: 4.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Here is a short description of the plugin.
+Editable manual for Admin panel
 
 == Description ==
 
